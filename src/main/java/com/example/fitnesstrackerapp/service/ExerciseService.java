2e0ba3a0ceef7899397e8c190717b984485d2c43
f.salamov <farhaad1987@gmail.com>
2024-01-08 23:50:00 +0400

    find all exercises

--- src/main/java/com/example/fitnesstrackerapp/service/ExerciseService.java
+++ src/main/java/com/example/fitnesstrackerapp/service/ExerciseService.java
@@ -2,8 +2,14 @@ package com.example.fitnesstrackerapp.service;
 
 import com.example.fitnesstrackerapp.dto.ExerciseDTO;
 import com.example.fitnesstrackerapp.entity.Exercise;
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public interface ExerciseService {
 
     Exercise save(ExerciseDTO exerciseDTO);
+
+    List<ExerciseDTO> findAllExercises();
 }
