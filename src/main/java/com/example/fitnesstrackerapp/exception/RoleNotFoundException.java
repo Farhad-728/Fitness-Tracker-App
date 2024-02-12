5a78a2d03675a86ad552975958479c1a1e5ff3d1
f.salamov <farhaad1987@gmail.com>
2024-02-12 21:26:29 +0400

    custom exception create

---
+++ src/main/java/com/example/fitnesstrackerapp/exception/RoleNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.fitnesstrackerapp.exception;
+
+public class RoleNotFoundException extends RuntimeException{
+    public RoleNotFoundException(String ex) {
+        super(ex);
+    }
+}
+
